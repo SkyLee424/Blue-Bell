cd4fbe561a7601d06147b317bc2a80f526eb530b
SkyLee424 <1350650238@qq.com>
2023-11-02 15:14:08 +0800

    Post 可以自动创建全文索引了

--- models/post.go
+++ models/post.go
@@ -6,8 +6,8 @@ type Post struct {
 	CommunityID int64  `gorm:"type:bigint;not null;" json:"community_id" binding:"required"`
 	AuthorID    int64  `gorm:"type:bigint;not null;" json:"author_id"`
 	Status      int8   `gorm:"type:tinyint;not null;default 1;" json:"status"`
-	Title       string `gorm:"type:varchar(128);not null;" json:"title" binding:"required"`
-	Content     string `gorm:"type:varchar(8192);not null;" json:"content" binding:"required"`
+	Title       string `gorm:"type:varchar(128);not null;index:idx_title,class:FULLTEXT" json:"title" binding:"required"`
+	Content     string `gorm:"type:varchar(8192);not null;index:idx_content,class:FULLTEXT,size:256" json:"content" binding:"required"`
 	CreatedAt   Time   `gorm:"type:timestamp default CURRENT_TIMESTAMP" json:"created_at"`
 	UpdatedAt   Time   `gorm:"type:timestamp default CURRENT_TIMESTAMP" json:"update_at"`
 }
