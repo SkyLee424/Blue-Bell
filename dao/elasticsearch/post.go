a2972973d61986cfb7ee1802fc4aa030ffccbc87
SkyLee424 <1350650238@qq.com>
2023-12-09 11:16:49 +0800

    修复了搜索结果为空，但返回错误的 bug

--- dao/elasticsearch/post.go
+++ dao/elasticsearch/post.go
@@ -2,6 +2,7 @@ package elasticsearch
 
 import (
 	bluebell "bluebell/errors"
+	"bluebell/logger"
 	"bluebell/models"
 	"context"
 	"encoding/json"
@@ -187,7 +188,8 @@ func getPostIDsHelper(query string, pageNum, pageSize int64) ([]string, int, err
 
 	start := (pageNum - 1) * pageSize
 	end := start + pageSize
-	if int(start) >= len(postIDs) { // 错误的分页参数
+	if start != 0 && int(start) >= len(postIDs) { // 错误的分页参数（注意排除搜索结果为空的情况）
+		logger.Debugf("start:%v, end:%v", start, end)
 		return nil, 0, errors.Wrap(bluebell.ErrInvalidParam, "elasticsearch: wrong paging parameters")
 	}
 	if int(end) > len(postIDs) {
