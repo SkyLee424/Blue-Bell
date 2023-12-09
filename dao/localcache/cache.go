f4cb55e2408bdcb77b3a5e89fc25f357dd6bd5f8
SkyLee424 <1350650238@qq.com>
2023-12-09 11:32:52 +0800

    使用切片作为存储 hot post 的数据结构，而不是 map（保证按热度排序）

--- dao/localcache/cache.go
+++ dao/localcache/cache.go
@@ -5,34 +5,25 @@ import (
 	"sync"
 )
 
-var hotPostCache map[int64]*models.PostDTO
+var hotPostCache []*models.PostDTO
 
 var hotPostCacheLock sync.RWMutex
 
 func init() {
-	hotPostCache = make(map[int64]*models.PostDTO)
+	hotPostCache = make([]*models.PostDTO, 0)
 }
 
 func GetHotPostDTOList() ([]*models.PostDTO, error) {
 	hotPostCacheLock.RLock() // 上读锁
 	defer hotPostCacheLock.RUnlock()
 
-	list := make([]*models.PostDTO, 0, len(hotPostCache))
-	for _, postDTO := range hotPostCache {
-		list = append(list, postDTO)
-	}
-
-	return list, nil
+	return hotPostCache, nil
 }
 
+// 参数的 hotPosts 已经按照热度排序
 func SetHotPostDTO(hotPosts []*models.PostDTO) {
-	newHotPostCache := make(map[int64]*models.PostDTO)
-	for _, hotPost := range hotPosts {
-		newHotPostCache[hotPost.PostID] = hotPost
-	}
-
 	hotPostCacheLock.Lock()
 	defer hotPostCacheLock.Unlock()
 
-	hotPostCache = newHotPostCache
+	hotPostCache = hotPosts
 }
