b9daa787fa0bf3b749870bc94d722046eac67a17
SkyLee424 <1350650238@qq.com>
2023-12-17 15:44:06 +0800

    添加了一个转换工具

--- internal/utils/strings.go
+++ internal/utils/strings.go
@@ -1,6 +1,9 @@
 package utils
 
-import "unicode/utf8"
+import (
+	"strconv"
+	"unicode/utf8"
+)
 
 func Substr(s string, start int, length int) string {
 	// 将字符串转换为rune切片以处理Unicode字符
@@ -17,14 +20,24 @@ func Substr(s string, start int, length int) string {
 	// 获取子字符串的起始和结束位置
 	substrStart := start
 	substrEnd := start + length
-	
+
 	if substrEnd > strLen {
 		substrEnd = strLen // 限制最大长度
 	}
-	
+
 	// 从rune切片中提取子字符串
 	substr := runes[substrStart:substrEnd]
 
 	// 将rune切片转换为字符串并返回
 	return string(substr)
+}
+
+func ConvertInt64SliceToStringSlice(arr []int64) []string {
+	res := make([]string, 0, len(arr))
+	for _, commentID := range arr {
+		arrtr := strconv.FormatInt(commentID, 10)
+		res = append(res, arrtr)
+	}
+
+	return res
 }
