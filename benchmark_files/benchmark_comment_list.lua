d4bb084dbcf038e7a7a9375e510c298e2c29ff2d
SkyLee424 <1350650238@qq.com>
2024-01-14 15:16:23 +0800

    更新了 Benchmark 的测试结果

--- benchmark_files/benchmark_comment_list.lua
+++ benchmark_files/benchmark_comment_list.lua
@@ -1,5 +1,5 @@
 wrk.method = "GET"
-wrk.path = "/api/v1/comment/list?obj_id=33120778639642624&obj_type=1&orderby=floor&page=1&size=20"
+wrk.path = "/api/v1/comment/list?obj_id=33120778639642624&obj_type=1&orderby=floor&page=1&size=50"
 wrk.headers["Content-Type"] = "application/json"
 wrk.headers["Accept"] = "application/json"
 wrk.headers["Authorization"] = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo5NTQxNTExNTc4MzkwNTI4LCJpc3MiOiJTa3lfTGVlIiwiZXhwIjoxNzA1OTk1MDE1fQ.dgtp0lCEbG_0MUdI7gWq6kpcH7oJiD2XvDo0oZ5PTVQ"
