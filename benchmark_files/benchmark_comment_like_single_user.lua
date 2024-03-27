367297323b67900c64830b81f1921483aa258f98
SkyLee424 <1350650238@qq.com>
2024-03-27 11:37:46 +0800

    更新了 README 和基准测试文件

---
+++ benchmark_files/benchmark_comment_like_single_user.lua
@@ -0,0 +1,5 @@
+wrk.method = "POST"
+wrk.path = "/api/v1/comment/like?comment_id=59829467639451648&obj_id=58139402215362560&obj_type=1"
+wrk.headers["Content-Type"] = "application/json"
+wrk.headers["Accept"] = "application/json"
+wrk.headers["Authorization"] = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo5NTQxNTExNTc4MzkwNTI4LCJpc3MiOiJTa3lfTGVlIiwiZXhwIjoxNzEyMzY5NDI0fQ.t7vP6IpVpUUCQwcBzyKxHai3W1zFnOOUpHwfz1HYehk"
